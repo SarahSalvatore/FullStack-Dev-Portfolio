2765534b816f59732c39cb643dd9d11a1a49093c
Sarah Salvatore <sarah.h.salvatore@gmail.com>
2023-02-02 13:50:03 -0500

    Add Condensed project to project list.

--- src/components/layout/ProjectItem.jsx
+++ src/components/layout/ProjectItem.jsx
@@ -1,15 +1,16 @@
-import { HashLink as Link } from "react-router-hash-link";
 import Badge from "../badges/Badge";
 
 const ProjectItem = ({ name, image, badges, desc, demoHref, repoHref }) => {
   return (
     <div className="section-container">
       <div className="project-left">
-        <img
-          className="project-img"
-          src={require(`../../assets/images/${image}`)}
-          alt="Wordle"
-        />
+        <a href={demoHref} target="_blank" rel="noreferrer">
+          <img
+            className="project-img"
+            src={require(`../../assets/images/${image}`)}
+            alt="project screenshot"
+          />
+        </a>
       </div>
       <div className="project-right">
         <h1 className="small-heading">{name}</h1>
--- src/data/projectList.js
+++ src/data/projectList.js
@@ -1,7 +1,7 @@
 export const projectList = [
   {
     index: 1,
-    tags: ["React", "JavaScript", "Sass", "HTML", "API"],
+    tags: ["React", "JavaScript", "Sass", "HTML", " Public API"],
     projectName: "Wordle Game",
     projectDesc:
       "The popular word guessing game built with React Hooks, a 2000+ word bank, and Free Dictionary API. The game features a rules modal, colour hints for correct/incorrect letters, and a game end modal displaying the correct word and it's definition. The site is fully responsive and deployed with GitHub Pages. ",
@@ -11,6 +11,24 @@ export const projectList = [
   },
   {
     index: 2,
+    tags: [
+      "JavaScript",
+      "Node",
+      "Express",
+      "MongoDB",
+      "Mongoose",
+      "CSS",
+      "HTML",
+    ],
+    projectName: "Condensed URL Shortener",
+    projectDesc:
+      "Transforms long URLs into more concise, 7 digit slugs. The condensed url links can be used anywhere and will redirect to the original url.",
+    projectImg: "condensed-screenshot.jpg",
+    demoLink: "https://condensed.netlify.app/",
+    repoLink: "https://github.com/SarahSalvatore/Condensed-server",
+  },
+  {
+    index: 3,
     tags: [
       "React",
       "JavaScript",

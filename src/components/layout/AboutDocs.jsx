3267756d7c80cd7a886c371da72ceac58adffef7
Sarah Salvatore <sarah.h.salvatore@gmail.com>
2023-04-04 10:02:54 -0400

    Update about links.

--- src/components/layout/AboutDocs.jsx
+++ src/components/layout/AboutDocs.jsx
@@ -1,14 +1,14 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faFile,
-  faAward,
-  faCertificate,
-} from "@fortawesome/free-solid-svg-icons";
+// import {
+//   faFile,
+//   faAward,
+//   faCertificate,
+// } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin, faSquareGithub } from "@fortawesome/free-brands-svg-icons";
 import { useInView } from "react-intersection-observer";
-import certificate from "../../assets/documents/ssalvatoreLetterCompletion2022.pdf";
-import resume from "../../assets/documents/SSalvatoreResume2023.pdf";
-import grades from "../../assets/documents/ssalvatoreGradesTranscript2022.pdf";
+// import certificate from "../../assets/documents/ssalvatoreLetterCompletion2022.pdf";
+// import resume from "../../assets/documents/SSalvatoreResume2023.pdf";
+// import grades from "../../assets/documents/ssalvatoreGradesTranscript2022.pdf";
 
 const AboutDocs = () => {
   const { ref, inView } = useInView({
@@ -21,54 +21,6 @@ const AboutDocs = () => {
         className={`docs-container ${inView ? "fade-fast" : null}`}
         ref={ref}
       >
-        <div className="doc-link">
-          <a
-            href={resume}
-            download="SSalvatoreResume2023.pdf"
-            target="_blank"
-            rel="noreferrer"
-            className="skills-badge link"
-          >
-            <FontAwesomeIcon
-              icon={faFile}
-              className="icon"
-              title="download resume"
-            />
-            Resume
-          </a>
-        </div>
-        <div className="doc-link">
-          <a
-            href={grades}
-            download="SSalvatoreTranscript.pdf"
-            target="_blank"
-            rel="noreferrer"
-            className="skills-badge link"
-          >
-            <FontAwesomeIcon
-              icon={faAward}
-              className="icon"
-              title="download transcript"
-            />
-            Grades
-          </a>
-        </div>
-        <div className="doc-link">
-          <a
-            href={certificate}
-            download="ssalvatoreLetterofCompletion.pdf"
-            target="_blank"
-            rel="noreferrer"
-            className="skills-badge link"
-          >
-            <FontAwesomeIcon
-              icon={faCertificate}
-              className="icon"
-              title="download certificate"
-            />
-            Certificate
-          </a>
-        </div>
         <div className="doc-link">
           <a
             href="https://www.linkedin.com/in/sarah-salvatore-full-stack-developer/"

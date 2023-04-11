e2b0f2d0b418da5c7d9d26666c5dd37789866198
Sarah Salvatore <sarah.h.salvatore@gmail.com>
2023-04-11 13:10:46 -0400

    Remove image from header.

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,12 +1,8 @@
 import { useInView } from "react-intersection-observer";
 import { HashLink as Link } from "react-router-hash-link";
-import heroImage from "../../assets/images/laptop.png";
 
 const Header = () => {
   const { ref: textRef, inView: textInView } = useInView({
-    triggerOnce: true,
-  });
-  const { ref: imgRef, inView: imgInView } = useInView({
     triggerOnce: true,
   });
 
@@ -18,24 +14,12 @@ const Header = () => {
           ref={textRef}
         >
           <div className="hero-text-container">
-            <h1 className="hero-heading-h1">Sarah</h1>
-            <h1 className="hero-heading-h1">Salvatore</h1>
+            <h1 className="hero-heading-h1">Sarah Salvatore</h1>
             <h2 className="hero-heading-h2">Full Stack Developer</h2>
             <Link to="/#skills" className="black-button">
               Learn More
             </Link>
           </div>
-        </div>
-        <div
-          className={`header-right ${imgInView ? "fade-up" : null}`}
-          ref={imgRef}
-        >
-          <img
-            src={heroImage}
-            className="hero-image"
-            alt="A laptop and tablet device with transparent screens projected around them."
-            title="Hello World!"
-          ></img>
         </div>
       </div>
     </header>

0426e7da6135c0088d60aebac1ba11fbe48d5f2b
Sarah Salvatore <sarah.h.salvatore@gmail.com>
2023-07-17 12:21:09 -0400

    Adjust section headings.

--- src/components/layout/SectionHeading.jsx
+++ src/components/layout/SectionHeading.jsx
@@ -1,10 +1,14 @@
 import { useInView } from "react-intersection-observer";
 
 const SectionHeading = ({ title }) => {
-  const { ref, inView } = useInView();
+  const { ref, inView } = useInView({ threshold: 0.2, triggerOnce: true });
 
   return (
-    <div className="section-head" ref={ref}>
+    <div
+      className="section-head"
+      ref={ref}
+      style={{ visibility: inView ? "visible" : "hidden" }}
+    >
       <h1 className={`section-heading-font ${inView ? "fade-down" : null}`}>
         {title}
       </h1>
